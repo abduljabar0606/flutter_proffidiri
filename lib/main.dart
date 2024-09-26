e3fe1be1ec73845a249116c09155f896c5147826
abduljabar0606 <abd.jabar0306@gmail.com>
2024-09-26 09:05:52 +0700

    Profdiri

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'page2.dart'; // Tambahkan impor ke second_page.dart
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profile Card Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile Card"),
+      ),
+      body: Center(
+        child: Card(
+          elevation: 4.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/images/prof1.jpg'), // Ganti dengan nama file gambar
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Muhamad Abdul Jabar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Murid SMK WIKRAMA BOGOR',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const SecondPage()),
+                    );
+                  },
+                  child: const Text('See More'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
